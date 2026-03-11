b915ca75bdbb1d70b64bf50c22bf29130d0a6305
Itgel Oyunbold <itgel.o@techpack.mn>
2026-03-11 15:40:50 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `
+          if (typeof crypto !== 'undefined' && typeof crypto.randomUUID !== 'function') {
+            crypto.randomUUID = function() {
+              return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, function(c) {
+                return (c ^ (Math.random() * 16 >> c / 4)).toString(16);
+              });
+            };
+          }
+        `}} />
+      </head>
       <body className={`${geistSans.variable} antialiased`} suppressHydrationWarning>
         {children}
       </body>
